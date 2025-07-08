55a69c021f44ccb925de5d1cb28b630ee70937e6
TheWritter-cell <abeavebejoe@gmail.com>
2025-07-08 21:14:59 +0200

    Finish the first structure of datamanger.cpp i will add other function soon

--- datamanager.cpp
+++ datamanager.cpp
@@ -7,25 +7,69 @@
 #include <random>
 #include <chrono>
 #include <sstream>
+#include <cstdio>
 #include <iomanip>
-std::string execPythonScript(){
-    std::array<char,128> buffer;
+#include <array>
+
+// Ik know there are too much include but i'm not a genius i use what i find in the internet chatgpt not really helpful :(
+
+// This function convert everything into a string i'm proud i made it by myself :)
+template<typename T>
+std::string to_str(const T& val){
+    std::ostringstream oss;
+    oss << val;
+    return oss.str();
+}
+
+// this function will collect argument and return it in string
+// i didn't made it i found it on github
+template<typename... Args>
+std::vector<std::string> collect_args(Args&&... args){
+    return {to_str(std::forward<Args>(args))...};
+}
+/** This function have been made by me after 3days of research so if you found how to ameliorate it
+Please do it because it's realy hard to maintain*/ 
+template<typename... Args>
+std::string execPythonScript(Args&&... args) {
+    std::vector<std::string> arglist = collect_args(std::forward<Args>(args)...);
+    std::size_t nb_args = sizeof...(args);
+    if (nb_args > 3) {
+        assert(false && "Too few arguments; you can only send 3 ");
+    }
+
+    std::array<char, 128> buffer;
     std::string result;
-    std::unique_ptr<FILE,decltype(&pclose)> pipe(popen("python python_called.py","r"),pclose);
-    if(!pipe){
-        throw std::runtime_error("Error during the python execution code");
+    std::ostringstream oss;
+    std::string command = "";
+
+    if (arglist[0] == "crypt" && nb_args == 2) {
+        oss << "python python_utilities/python_tools.py " << arglist[0] << " " << arglist[1];
+        command = oss.str();
+    } else if (arglist[0] == "check" && nb_args == 3) {
+        oss << "python python_utilities/python_tools.py " << arglist[0] << " " << arglist[1] << " " << arglist[2];
+        command = oss.str();
+    } else {
+        return "NONE";
     }
-    while (fgets(buffer.data(),buffer.size(),pipe.get())!=nullptr){
-        result+=buffer.data();
+
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(command.c_str(), "r"), pclose);
+    if (!pipe) {
+        throw std::runtime_error("Error during the python execution");
     }
-    if(!result.empty() && result.back()=='\n'){
+
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+
+    if (!result.empty() && result.back() == '\n') {
         result.pop_back();
     }
+
     return result;
 }
 
-// Compile with:
-// g++ try.cpp -o try -I "/c/Program Files (x86)/SQLiteCpp/include" -L "/c/Program Files (x86)/SQLiteCpp/lib" -lSQLiteCpp -lsqlite3 
+// Compile with on MinGW64 terminal i'm on windows for linux i don't know
+// g++ datamanager.cpp -o db -I "/c/Program Files (x86)/SQLiteCpp/include" -L "/c/Program Files (x86)/SQLiteCpp/lib" -lSQLiteCpp -lsqlite3 
 
 class DataManager {
 private:
@@ -91,15 +135,21 @@ public:
         return ss.str();
     }
     // Code For add new user in the table
-    /*int InsertUser(std::string name,const char* password ,long long int balance=0){
+    int InsertUser(std::string name,const char* password ,long long int balance=0){
         SQLite::Statement insert(db,"INSERT INTO USER (id,NAME , BALANCE,PASSWORD) VALUES (?,?,?,?)");
         try{
             std::string id=generateID();
+            std::string hash_psw=execPythonScript("crypt",password);
+            insert.bind(1,id);
+            insert.bind(2,name);
+            insert.bind(3,balance);
+            insert.bind(4,hash_psw);
+            insert.exec();
             return 0;
         }catch(std::exception& e){
             assert(false && e.what());
         }
-    }*/
+    }
 };
 
 int main() {
@@ -108,8 +158,7 @@ int main() {
         DataManager db("database.db");
 
         // Create the Users table
-        db.CreateTable("admin");
-        db.CreateTable("Users");
+        db.InsertUser("pai","ijio",450);
 
     } catch (const std::exception& e) {
         // Handle any exceptions thrown by SQLite
--- python_utilities/python_tools.py
+++ python_utilities/python_tools.py
@@ -4,9 +4,9 @@ import sys
 def crypt(password:str):
     try:
         hash_psw=bcrypt.hashpw(password.encode(),bcrypt.gensalt())
-        return hash_psw
+        return hash_psw.decode()
     except Exception as ex:
-        return bytes(0)
+        return "0"
 
 def checkpsw(hash_password:str,password:str):
     if(bcrypt.checkpw(password.encode(),hash_password.encode())):
@@ -14,18 +14,12 @@ def checkpsw(hash_password:str,password:str):
     else:
         return "0"
 
-if __name__=="__name__":
-    if len(sys.argv)>3:
-        print("To fue argument")
-    elif len(sys.argv)>2:
-        if sys.argv[1].lower()=="crypt":
-            hash=crypt(sys.argv[2])
-            print(str(hash))
-        elif sys.argv[1].lower()=="check":
-            result=checkpsw(sys.argv[2],sys.argv[3])
-            print(result)
-        else:
-            print("Unknow operation")
+if __name__=="__main__":
+    if sys.argv[1].lower()=="crypt":
+        hash=crypt(sys.argv[2])
+        print(str(hash))
+    elif sys.argv[1].lower()=="check":
+        result=checkpsw(sys.argv[2],sys.argv[3])
+        print(result)
     else:
-        print("Not enough argument")
-    
+            print("Unknow operation")

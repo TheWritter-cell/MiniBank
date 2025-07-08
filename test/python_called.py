e0948fc5640a6480d276ab503b9907d9c063aa0d
TheWritter-cell <abeavebejoe@gmail.com>
2025-07-09 00:24:27 +0200

    Clean up des repo

--- test/python_called.py
+++
@@ -1,9 +0,0 @@
-import uuid
-import bcrypt
-
-def generate_code():
-    return str(uuid.uuid4())
-
-if __name__=="__main__":
-    password=bcrypt.hashpw("jifrjf".encode(),bcrypt.gensalt())
-    print(type(password))
